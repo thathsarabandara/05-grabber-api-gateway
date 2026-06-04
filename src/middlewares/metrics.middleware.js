d89d8987be58794bcd9e87f8b431da9fe6fbc4c1
Thathsara Bandara <thathsaraarumapperuma@gmail.com>
2026-06-04 17:47:30 +0530

    feat: implement health check routes, prometheus metrics middleware, and device database models

---
+++ src/middlewares/metrics.middleware.js
@@ -0,0 +1,36 @@
+const client = require('prom-client');
+
+// Create a Registry which registers the metrics
+const register = new client.Registry();
+
+// Add a default label which is added to all metrics
+register.setDefaultLabels({
+  app: 'grabber-api-gateway'
+});
+
+// Enable the collection of default metrics
+client.collectDefaultMetrics({ register });
+
+// Custom metric for HTTP request duration
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in microseconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
+});
+
+register.registerMetric(httpRequestDurationMicroseconds);
+
+const metricsMiddleware = (req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    const route = req.route ? req.route.path : req.path;
+    end({ method: req.method, route, code: res.statusCode });
+  });
+  next();
+};
+
+module.exports = {
+  register,
+  metricsMiddleware
+};
